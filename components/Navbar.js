83ba36ce60ed0aa016430efeb275a2d04dd03a6a
spero61 <spero61@gmail.com>
2022-07-10 18:23:35 +0900

    Fix Warning: Function components cannot be given refs.

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,17 +2,15 @@ import Link from 'next/link';
 import Image from 'next/image';
 import blogLogo from '../public/blog-logo.svg';
 import { useRouter } from 'next/router';
-// import { useContext } from 'react';
-// import { UserContext } from '@lib/context';
-// import { auth } from '@lib/firebase';
+import { useContext } from 'react';
+import { UserContext } from '../lib/context';
+import { auth } from '../lib/firebaseConfig';
 import { signOut } from 'firebase/auth';
 
-// Top navbar
-export default function Navbar() {
-  //   const { user, username } = useContext(UserContext);
+// FAQ: https://nextjs.org/docs/api-reference/next/link#if-the-child-is-a-functional-component
 
-  const username = true;
-  const user = true;
+export default function Navbar() {
+  const { user, username } = useContext(UserContext);
 
   const router = useRouter();
 
@@ -24,18 +22,13 @@ export default function Navbar() {
   return (
     <nav className="bg-white border-gray-200 px-3 py-3 md:px-4 py-3 sticky">
       <div className="flex flex-wrap justify-between items-center">
+        <span className="hover:cursor-pointer">
           <Link href="/">
-            <Image id="blog-logo" src={blogLogo} width={126} height={36} alt="Blog Logo" />
+            <a>
+              <Image src={blogLogo} width={126} height={36} alt="Blog Logo" />
+            </a>
           </Link>
-        {/* <a href="https://flowbite.com/" className="flex items-center">
-      <img src="/docs/images/logo.svg" className="mr-3 h-6 md:h-9" alt="Flowbite Logo" />
-      <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-      </a> */}
-        {/* <li>
-          <Link href="/">
-            <Image src={blogLogo} alt="Blog Logo"></Image>
-          </Link>
-        </li> */}
+        </span>
 
         {/* the user is successfully "signed-in" and has "username" */}
         {username && (
@@ -48,27 +41,36 @@ export default function Navbar() {
             </button>
 
             <Link href="/admin">
-              <button className="text-stone-100 bg-fuchsia-500 hover:bg-fuchsia-800 rounded-full font-medium text-xs px-2 py-2.5 py-2 mx-1 md:text-base md:px-5 md:py-2.5 md:mx-3">
+              <button className="text-stone-100 bg-orange-500 hover:bg-orange-700 rounded-full font-medium text-xs px-2 py-2.5 py-2 mx-1 md:text-base md:px-5 md:py-2.5 md:mx-3">
                 Write Posts
               </button>
             </Link>
-            <Link href={`/${username}`}>
-              <Image
-                src={user?.photoURL || '/default_avatar.png'}
-                width={45}
-                height={45}
-                alt="User Profile Photo"
-              />
-            </Link>
+            <span className="hover:cursor-pointer">
+              <Link href={`/${username}`}>
+                <a>
+                  <Image
+                    src={user?.photoURL || '/default_avatar.png'}
+                    width={45}
+                    height={45}
+                    alt="User Profile Photo"
+                  />
+                </a>
+              </Link>
+            </span>
           </div>
         )}
 
         {/* the user is "not signed-in" || "does not have username" yet */}
         {!username && (
-              <Link href="/enter">
-                <button type="button" className="text-stone-100 bg-fuchsia-500 hover:bg-fuchsia-800 rounded-full font-medium text-xs px-2 py-2.5 py-2 mx-1 md:text-base md:px-7 md:py-2.5 md:mx-3">Login</button>
-              </Link>
-          )}
+          <Link href="/enter">
+            <button
+              type="button"
+              className="text-stone-100 bg-orange-500 hover:bg-orange-800 rounded-full font-medium text-xs px-2 py-2.5 py-2 mx-1 md:text-base md:px-7 md:py-2.5 md:mx-3"
+            >
+              Login
+            </button>
+          </Link>
+        )}
       </div>
     </nav>
   );
